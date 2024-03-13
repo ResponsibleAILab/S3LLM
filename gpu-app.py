bf8fe50198ad35c581f29ee4b84677607488da23
Shaik <ks1249@lxhvxnry3-cse.unt.ad.unt.edu>
2024-03-13 03:50:14 +0000

    updated readme.md

--- gpu-app.py
+++ gpu-app.py
@@ -7,7 +7,6 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 from langchain.chains import RetrievalQA
